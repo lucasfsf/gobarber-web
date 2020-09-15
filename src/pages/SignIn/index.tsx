3db4b7677844c9efc04c2b5e2ce960828c3c579c
Lucas Fonseca <lucasfreireri@gmail.com>
2020-09-15 08:37:56 -0300

    Added Context API

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,10 +1,11 @@
 // eslint-disable-next-line no-use-before-define
-import React, { useRef, useCallback } from 'react';
+import React, { useRef, useCallback, useContext } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
 
+import AuthContext from '../../context/AuthContext';
 import getValidationErrors from '../../utils/getValidationErrors';
 
 import logoImg from '../../assets/logo.svg';
@@ -16,6 +17,9 @@ import { Container, Content, Background } from './styles';
 
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
+
+  const { name } = useContext(AuthContext);
+  console.log(name);
 
   const handleSubmit = useCallback(async (data: object) => {
     try {
